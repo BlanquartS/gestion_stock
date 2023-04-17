4bb8ca2d24d924039f4c31c11d516b39d6a795ac
Simon blanquart <simon.bl@hotmail.fr>
2023-04-18 01:59:52 +0200

    add key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl ="https://hdlahckfingtktevnsnr.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkbGFoY2tmaW5ndGt0ZXZuc25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU3MjQ4MjQsImV4cCI6MTk5MTMwMDgyNH0.6ihVubLYvjdYd8U3Pq1IrMXw8C40tQ9wctp6DAmEo1c"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
